acec8fd40379008fbd78d951d707f6509eeff6f7
Lhimbo <32436536+Lhimbo@users.noreply.github.com>
2020-09-26 13:31:04 -0300

    Add files via upload

---
+++ webapijoke/classes/Flags.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace webapijoke.classes
+{
+    class Flags
+    {
+        public bool nsfw { get; set; }
+        public bool religious { get; set; }
+        public bool racist { get; set; }
+        public bool sexist { get; set; }
+        public bool political { get; set; }
+
+        public string all()
+        {
+            return $"NSFW = {nsfw} Religious = {religious} Racist = {racist} Sexist = {sexist} Political = {political}";
+        }
+    }
+}
---
+++ webapijoke/classes/Joke.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace webapijoke.classes
+{
+    class Joke
+    {
+        public bool error { get; set; }
+        public string category { get; set; }
+        public string type { get; set; }
+        public string joke { get; set; }
+        public Flags flags { get; set; }
+        public int id { get; set; }
+        public string lang { get; set; }
+
+        public void Show()
+        {
+            Console.WriteLine($"ERRO : {error}." +
+                              $"\nCategory : {category}." +
+                              $"\nType : {type}" +
+                              $"\nFlags : {flags.all()}" +
+                              $"\nID : {id}" +
+                              $"\nLang : {lang}");
+            Console.WriteLine($"\n\n\nJoke : \n{joke}");
+        }
+    }
+}
