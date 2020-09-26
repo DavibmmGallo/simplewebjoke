65d6f89f998f36d0dd78e012cd2396d3c0253d44
Lhimbo <32436536+Lhimbo@users.noreply.github.com>
2020-09-26 13:34:27 -0300

    Update Program.cs

--- webapijoke/Program.cs
+++ webapijoke/Program.cs
@@ -16,17 +16,22 @@ namespace webapijoke
         static async void mainthread()
         {
             HttpClient httpClient = new HttpClient();
-
+            
+            //url
             string url = $"https://sv443.net/jokeapi/v2/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist&type=single";
-
+            
+            //Get post
             HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
             HttpResponseMessage response = httpClient.SendAsync(request).Result;
+            
             string dados = await response.Content.ReadAsStringAsync();
-
+            
+            //Deserializing json into Joke class
             Joke myjoke = JsonConvert.DeserializeObject<Joke>(dados);
 
             myjoke.Show();
-
+            
+            //Save joke as json at app directory
             char c = '0';
             Console.Write("\nDo you like this joke, save it! (y/n) : ");
             char.TryParse(Console.ReadLine(), out c);
